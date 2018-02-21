a3135a76b6154437755c7e5ce0f66bbdf04a2339
David BLANCO PARADA <david.blancop@gmail.com>
2018-02-21 10:59:47 +0100

    Add input/output def template

--- hash2018.py
+++ hash2018.py
@@ -4,8 +4,34 @@ Created on the 19th of February, 2018
 @author: 'Per Kata Ad Astra' Team
 '''
 
+def parse_input():
+
+    f = open("file.in","r")
+
+    ## Read one line
+    line = f.readline()
+    print(line)
+
+    ## Read all lines in file
+    for line in f:
+        print(line)
+
+    f.close()
+
+
+def dump_output():
+
+    f = open("file.out","w")
+
+    ## Write stuff
+    f.write("%s\n" % "Some stuff to write in out file")
+
+    f.close()
+
+
 def main():
     print("!!!!Google Hash 2018!!!!")
+
 
 if __name__ == '__main__':
     main()
