65022c15f0f54bb563dc933cc801d446d23cab18
Henrik Eideberg <henrik@eideberg.se>
2018-03-01 19:38:18 +0100

    save first row in global variables

--- hash2018.py
+++ hash2018.py
@@ -3,19 +3,35 @@ Created on the 19th of February, 2018
 
 @author: 'Per Kata Ad Astra' Team
 '''
+import sys
 
-def parse_input():
+numberOfRows = 0
+numberOfColumns = 0
+numberOfVehicles = 0
+numberOfRides = 0
+perRideBonus = 0
+numberOfSimSteps = 0
 
-    f = open("file.in","r")
+def parse_input(inputFile):
+
+    f = open(inputFile,"r")
 
     ## Read one line
-    line = f.readline()
-    print(line)
+    #line = f.readline()
+    #print(line)
 
     ## Read all lines in file
-    for line in f:
-        print(line)
+    #for line in f:
+    #    print(line)
 
+    listWithAllLines = f.readlines()
+    firstRow = listWithAllLines[0].split()
+    numberOfRows = int(firstRow[0])
+    numberOfColumns = int(firstRow[1])
+    numberOfVehicles = int(firstRow[2])
+    numberOfRides = int(firstRow[3])
+    perRideBonus = int(firstRow[4])
+    numberOfSimSteps = int(firstRow[5])
     f.close()
 
 
@@ -38,6 +54,11 @@ def get_dist(rs, cs, re, ce):
 
 def main():
     print("!!!!Google Hash 2018!!!!")
+    if len(sys.argv) >= 2:
+      fileInput = sys.argv[1]
+    else:
+      fileInput = "a_example.in"
+      parse_input(fileInput)
 
 
 if __name__ == '__main__':
